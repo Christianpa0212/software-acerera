88a79eedf8766aa523d1b208c3665511eca1e903
Christian <perezalfarochristian248@gmail.com>
2025-03-24 23:24:06 -0600

    Fase Inicial: Se configuro el servidor y la base de datos.

---
+++ server/server.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const xss = require('xss-clean');
+
+// Importar la conexión a la base de datos
+require('./config/db/db');
+
+const app = express();
+
+//Middlewares de seguridad
+app.use(helmet());
+app.use(xss()); 
+app.use(cors());    
+app.use(morgan('dev'));  
+app.use(express.json()); 
+
+//Limita número de peticiones por IP (anti-bruteforce)
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+app.use(limiter);
+
+//Rutas de prueba o principales (esto lo modularás después)
+app.get('/api/ping', (req, res) => {
+  res.json({ message: 'Servidor funcionando correctamente' });
+});
+
+//Inicializa el servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto: ${PORT}`);
+});
